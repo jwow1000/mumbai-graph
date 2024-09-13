cabd1645f9cda72bd4eb49648cbc856cf43b5dba
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2024-09-13 19:05:01 -0400

    xaxis is categorical, y is range, line can draw over the dates and categories

--- main.js
+++ main.js
@@ -1,34 +1,82 @@
 import * as d3 from "d3";
 
-const container = document.querySelector('#app');
+const container = document.getElementById('graph-contain');
 
 // Declare the chart dimensions and margins.
 const width = 800;
-const height = 400;
-const marginTop = 20;
+const height = 1400;
+const marginTop = 180;
 const marginRight = 20;
 const marginBottom = 30;
 const marginLeft = 40;
+const innerLeft = 30;
+const innerTop = 40;
+
 const numCols = 12;
 const numRows = 11;
 
+
+// x-axis define
+const xAxisDef = {
+  planning: [
+    "NA CONVERSION",
+    "PURCHASING OF LAND",
+    "SURVEY",
+    "VERIFICATION",
+    "PLAN MAKING",
+    "PLAN APPROVAL",
+    "PLAN VERIFICATION",
+  ],
+  infastructure: [
+    "ELECTRICITY",
+    "SEWAGE",
+    "DRAINAGE",
+    "TOILET",
+    "WATER SUPPLY",
+    "PUBLIC SPACES",
+  ],
+  building: [
+    "KACHHA HOUSE",
+    "PAKKA HOUSE",
+    "EXTENSIONS",
+    "RETROFITS",
+  ],
+  occupancy: [
+    "OWNERSHIP",
+    "RENTAL",
+    "PART RENTAL"
+  ]
+}
+
+// y axis range define
+const dateRange = d3.timeMonths( new Date("2002-01-01"), new Date("2024-01-01"), 6);
+
+const xAxisCat = [
+  ...xAxisDef.planning, 
+  ...xAxisDef.infastructure, 
+  ...xAxisDef.building, 
+  ...xAxisDef.occupancy
+]
+
 // test path coordinates
 const house1 = [
-  { date: new Date("2005-01-01"), value: 30 },
-  { date: new Date("2010-01-01"), value: 50 },
-  { date: new Date("2015-01-01"), value: 70 },
-  { date: new Date("2020-01-01"), value: 10 },
+  { date: new Date("2005-01-01"), category: "KACHHA HOUSE" },
+  { date: new Date("2010-01-01"), category: "PAKKA HOUSE" },
+  { date: new Date("2015-01-01"), category: "TOILET" },
+  { date: new Date("2020-01-01"), category: "OWNERSHIP" },
 ];
 
-// Declare the x (horizontal position) scale.
-const x = d3.scaleUtc()
-    .domain([new Date("2002-01-01"), new Date("2024-01-01")])
-    .range([marginLeft, width - marginRight]);
 
-// Declare the y (vertical position) scale.
-const y = d3.scaleLinear()
-    .domain([0, 100])
-    .range([height - marginBottom, marginTop]);
+// make a planning x axis
+const x = d3.scaleBand()
+  .domain( xAxisCat )
+  .range( [0, width] )
+  .padding( 0.1 )
+
+// Declare the y (horizontal position) scale.
+const y = d3.scaleUtc()
+  .domain([new Date("2002-01-01"), new Date("2024-01-01")])
+  .range([ marginLeft, height]);
 
 // Create the SVG container.
 const svg = d3.create("svg")
@@ -37,75 +85,53 @@ const svg = d3.create("svg")
 
 // Add the x-axis.
 svg.append("g")
-    .attr("transform", `translate(0,${height - marginBottom})`)
-    .call(d3.axisBottom(x));
+  .attr("transform", `translate(${ marginLeft + innerLeft }, ${ marginTop })`)
+  .call( d3.axisTop(x) )
+  .attr("class", "xAxis")
+  .selectAll("path, line")
+  .style("display", "none")
+
+// style the x axis
+svg.selectAll(".xAxis text")
+  .style("text-anchor", "start") // Center-align text
+  .attr("transform", "rotate(-90)") // Rotate labels 90 degrees
+  .attr("y", 0) // Move labels to the left to align correctly
+  .attr("x", 0) // Adjust horizontal positioning
+  .style("font-size", "10px")
 
 // Add the y-axis.
 svg.append("g")
-    .attr("transform", `translate(${marginLeft},0)`)
-    .call(d3.axisLeft(y));
+  .attr("transform", `translate(${marginLeft},${marginTop + innerTop})`)
+  .call( d3.axisLeft(y))
+  .attr("class", "yAxis")
+  .selectAll("path, line")
+  .style("display", "none")
 
-// make dot grid
-const xPositions = d3.range( numCols ).map(d => 
-  x.invert(
-    x.range()[0] + d * (x.range()[1] - x.range()[0]) / (numCols - 1 ))
-);
-const yPositions = d3.range( numRows ).map(d => y.invert(
-  y.range()[0] + d * (y.range()[1] - y.range()[0]) / (numRows - 1 ))
-);
 
-// append the dots to the SVG
-xPositions.forEach( xVal => {
-  yPositions.forEach( yVal => {
-    svg.append("circle")
-      .attr("cx", x(xVal))
-      .attr("cy", y(yVal))
-      .attr("r", 5)
-      .attr("fill", "steelBlue");
-  });
+// make the dot grid
+const data = xAxisCat.flatMap(cat => {
+  return dateRange.map(date => ({
+    category: cat,
+    date: date
+  }));
 });
 
+svg.selectAll(".dot")
+  .data( data )
+  .enter().append("circle")
+  .attr("class", "dot")
+  .attr("cx", d => x(d.category) + marginLeft + 15 + innerLeft)
+  .attr("cy", d => y(d.date) + marginTop + innerTop)
+  .attr("r", 6)
+  .style("fill", "none")
+  .style("stroke", "grey")
 
 
-    // // a function to make the dots
-// const dotsData = d3.range( rows*cols ).map(i => {
-//   const row = Math.floor( i / cols );
-//   const col = i % cols;
-  
-//   return {
-//     cx: (col * dotSpacing + dotRadius) + marginLeft,
-//     cy: (row * dotSpacing + dotRadius) + marginTop
-//   };
-// });
-
-// make the dots
-// svg.selectAll("circle")
-//      .data( dotsData )
-//      .enter()
-//      .append("circle")
-//      .attr("cx", d => d.cx)
-//      .attr("cy", d => d.cy)
-//      .attr("r", dotRadius)
-//      .attr("fill", "gray")
-
-//      // Add hover effect
-//      .on("mouseover", function() {
-//         d3.select(this)
-//           .classed("hovered", true)
-//           .attr("r", dotRadius * 3)
-//           .attr("fill", "orange");  // Increase radius on hover
-//     })
-//     .on("mouseout", function() {
-//         d3.select(this)
-//           .classed("hovered", false)
-//           .attr("r", dotRadius)  // Reset radius after hover
-//           .attr("fill", "blue");
-//     });
 
 // make the line
 const line = d3.line()
-  .x(d => x(d.date))
-  .y(d => y(d.value))
+  .x(d => x(d.category) + marginLeft + 15 + innerLeft)
+  .y(d => y(d.date) + marginTop + innerTop)
   .curve( d3.curveLinear);
 
 // draw the line
