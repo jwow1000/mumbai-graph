ed936220cf1e77b66a1dda0345e9ab6e15962875
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2024-10-03 15:06:35 -0400

    add labels

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ import * as d3 from "d3";
 // variables
 let focusState = false;
 let targetYear = 0;
+let sliding = false;
 
 // get the graph container from webflow
 const container = document.getElementById('graph-contain');
@@ -14,11 +15,11 @@ const container = document.getElementById('graph-contain');
 // variables
 // Declare the chart dimensions and margins.
 const width = 1800;
-const height = 900;
-const marginTop = 100;
+const height = 1000;
+const marginTop = 10;
 const marginRight = 20;
 const marginBottom = 50;
-const marginLeft = 120;
+const marginLeft = 150;
 const innerLeft = 40;
 const innerTop = 40;
 
@@ -27,21 +28,30 @@ const theHouses = getData();
 console.log("theHouses", theHouses)
 
 const yAxisCat = [
-  ...yAxisDef.occupancy, 
   "_spacer1",
-  ...yAxisDef.building, 
+  ...yAxisDef.occupancy, 
   "_spacer2",
-  ...yAxisDef.infastructure, 
+  ...yAxisDef.building, 
   "_spacer3",
+  ...yAxisDef.infastructure, 
+  "_spacer4",
   ...yAxisDef.planning,
-]
+];
+
+// Define labels for the spacers
+const spacerLabels = {
+  "_spacer1": "OCCUPANCY",
+  "_spacer2": "BUILDING",
+  "_spacer3": "INFASTRUCTURE",
+  "_spacer4": "PLANNING",
+};  
 
 // Create the SVG container
 const svg = d3.create("svg")
   .attr("width", width)
-  .attr("height", height);
+  .attr("height", height)
 
-  // Declare the x scale.
+// Declare the x scale.
 const xScale = d3.scaleLinear()
   .domain( [0, years.length] )
   .range( [ marginLeft + innerLeft, width - innerLeft] )
@@ -94,6 +104,7 @@ function calcXpos( target ) {
 
 // calculate new positions for new target year and redraw
 function reDrawElements( target ) {
+  sliding = true;
   // update target year
   // targetYear = target;
   xPositions = calcXpos( target );
@@ -138,7 +149,8 @@ function reDrawElements( target ) {
       .datum( item.listxy )
       .transition()
       .duration( 1000 )
-      .attr("d", lineGen( item.listxy ));
+      .attr("d", lineGen( item.listxy ))
+      .attr("stroke-width", 2)
     
   
     // redraw the circles 
@@ -153,8 +165,10 @@ function reDrawElements( target ) {
         const seasonSpace = d.season * getSeasonSpace( d );
         return (xPositions[d.year].position + innerLeft + marginLeft) + seasonSpace
       })
+      .on("end", function(){
+        sliding = false;
+      })
   });
-
 }
 
 // calculate x positions
@@ -176,20 +190,45 @@ xAxis.selectAll(".xLabel")
     .attr("text-anchor", "middle")
     .text(d => d.year)
 
+const globalYAxisPadding = 0.5;
 // declare the y axis scale
 const yScale = d3.scaleBand()
   .domain( yAxisCat )
   .range( [marginTop, height - marginBottom] )
-  .padding( 0 )
+  .padding( globalYAxisPadding )
 
 const yAxis = d3.axisLeft( yScale )
   .tickSize( 0 )
-  .tickPadding( 0)
+  .tickPadding( 0 )
   .tickFormat(function(d) {
     // Hide the placeholders by returning an empty string for them
     return d.startsWith("_spacer") ? "" : d;
   })
 ;
+
+const yAxisThemeScale = d3.scaleBand()
+  .domain( yAxisCat )
+  .range( [marginTop, height - marginBottom] )
+  .padding( globalYAxisPadding )
+
+
+const yAxisLabels = d3.axisLeft( yAxisThemeScale )
+  .tickSize( 0 )
+  .tickPadding( 0 )
+  .tickFormat(function(d) {
+    // Hide the placeholders by returning an empty string for them
+    return d.startsWith("_spacer") ? spacerLabels[d] : "";
+  })
+
+const yAxisLabelsGroup = svg.append("g")
+  .attr("class", "yAxisLabels")
+  .attr("transform", `translate(${ (marginLeft + innerLeft) - 30 }, ${ 0 })`)
+  .call( yAxisLabels )
+  
+    
+  
+
+
 
 // add the Y axis
 const yAxisGroup = svg.append("g")
@@ -203,6 +242,10 @@ svg.selectAll("path").remove();
 // style y axis labels
 yAxisGroup.selectAll("text")
   .style("font-size", "0.8rem")
+
+// create the Y theme name labels
+const yAxisThemes = svg.append("g")
+  .attr("class", "yThemes")
 
 // create the dot data
 let dotData = Object.values(xPositions).flatMap(item => {
@@ -261,7 +304,7 @@ theHouses.forEach( item => {
   console.log("listXY: ", item.listxy, xPositions)
   // draw the preview
   svg.append( "image" )
-    .attr("class", `preview-${item.name}`) 
+    .attr("class", `preview-${item.name} thumbs`) 
     .attr("x", xPositions[ item.listxy[0].year ].position + (item.previewPos[0] * width) )
     .attr("y", item.previewPos[1] * height )
     .attr("width", "100")
@@ -293,9 +336,33 @@ theHouses.forEach( item => {
     .attr("fill", "none")
     .attr("stroke", "black")
     .attr("stroke-width", 2)
-    .attr("d", lineGen( item.listxy ));
-  
+    .attr("d", lineGen( item.listxy ))
+    .on('click', function(event) {
+      d3.select(this)
+        .interrupt()
+        .transition()
+        .duration( 1000 )
+        .attr("stroke-width", 2)
+      zoomOnItem( item.listxy[0].year, item ); // Trigger the zoom/collapse effect
+    })
+    .on("mouseover", function(event) {
+      if( !sliding ) {
+        d3.select( this )
+        .transition()
+        .duration( 300 )
+        .attr("stroke-width", 4)
+      }
+    })
+    .on("mouseout", function(event) {
+      if( !sliding ) {
 
+        d3.select( this )
+          .transition()
+          .duration( 300 )
+          .attr("stroke-width", 2)
+      }
+    })
+  
   // add circles to the line
   svg.selectAll(".circles")
     .data( item.listxy )
@@ -317,20 +384,24 @@ theHouses.forEach( item => {
         .attr("r", 8)
       zoomOnItem( item.listxy[0].year, item ); // Trigger the zoom/collapse effect
     })
-    // .on("mouseover", function(event) {
-    //   d3.select( this )
-    //   .transition()
-    //   .duration( 500 )
-    //   .attr("r", 25)
-    // })
-    // .on("mouseout", function(event) {
-    //   d3.select( this )
-    //     .interrupt()
-    //     .transition()
-    //     .duration( 500 )
-    //     .attr("r", 8)
-    // })
-});
+    .on("mouseover", function(event) {
+      if( !sliding ) {
+        d3.select( this )
+        .transition()
+        .duration( 500 )
+        .attr("r", 12)
+      }
+    })
+    .on("mouseout", function(event) {
+      if( !sliding ) {
+
+        d3.select( this )
+          .transition()
+          .duration( 500 )
+          .attr("r", 8)
+      }
+    })
+  });
 
 // create image element for focused story, opacity 0 on init
 svg.append("image")
@@ -339,13 +410,75 @@ svg.append("image")
   .attr("y", 0)
   .attr("width", bigGap)   //bigGap is how wide the window is when focus
   .attr("height", height)
+  .attr("display", "none")
   .attr("opacity", 0)
   .attr("href", theHouses[0].displayImg )
-  .attr("pointer-events", "none")
+  // .attr("pointer-events", "none")
   .attr("preserveAspectRatio", "xMidYMid meet")
   .on("click", function( event ) {
-    zoomOnItem( );
+    if( focusState ) {
+      closeZoom();
+    }
   })
+    
+
+function closeZoom() {
+  console.log("turn off focus")
+  // redraw the scene with year 0 to return to default
+  reDrawElements( 0 );
+
+  // make the story image dissapear
+  svg.select(".story-image")
+    .transition()
+    .duration( 1000 )
+    .attr("opacity", 0)
+    .attr("display", "none")
+
+  // turn off focus state
+  focusState = false;
+}
+
+// add labels to yAxis
+yAxisLabelsGroup.selectAll('text')
+    .each( function() {
+      if( this.__data__.startsWith('_spacer') ) {
+        console.log("omg this", this)
+        // style font
+        d3.select( this )
+          .style("font-size", "1rem")
+          .style("font-weight", 700)
+          .style("text-align", "left")
+
+        
+        // add the box
+        const elem = d3.select(this).node();
+
+        console.log("bounding box", elem.parentNode.getBBox())
+
+        d3.select( this.parentNode )  // Select the parent group of the text (tick group)
+          .insert("rect", "text")     // Insert circle before the text
+          .attr("x",  -marginLeft)    // Same x position as text
+          .attr("y",  -16)            // Same y position as text
+          .attr("width", "13rem" )  
+          .attr("height", "2rem" )  
+          .attr("rx", 6)  // Rounded corner radius
+          .attr("ry", 6)
+          .style("fill", "none")  // Background color
+          .style("stroke", "black")  // Border color
+          .style("stroke-width", 1);  // Border width
+        
+        d3.select( this.parentNode )
+          .insert("line", "rect")
+          .attr("x1", "4rem")  // Starting x position
+          .attr("y1", 0)  // Starting y position (same as rect)
+          .attr("x2", width)  // Ending x position (adjust as needed)
+          .attr("y2", 0)  // Ending y position (same as rect) 
+          .style("stroke", "black")  // Color of the dashed line
+          .style("stroke-width", 1)  // Width of the dashed line
+          .style("stroke-dasharray", ("3, 3")) 
+      }
+
+    })
 
 function zoomOnItem( target, item ) {
   // console.log("xPositions before click", xPositions);
@@ -358,6 +491,7 @@ function zoomOnItem( target, item ) {
     svg.select(".story-image")
       .attr("x", xPositions[target].position + innerLeft + marginLeft)     // reset to new x position
       .attr("href", item.displayImg)
+      .attr("display", "block")
       .transition()
       .duration( 1000 )
       .attr("opacity", 1);
@@ -366,21 +500,12 @@ function zoomOnItem( target, item ) {
     focusState = true;
 
   } else {
-    // redraw the scene with year 0 to return to default
-    reDrawElements( 0 );
-
-    // make the story image dissapear
-    svg.select(".story-image")
-      .transition()
-      .duration( 1000 )
-      .attr("opacity", 0);
-
-    // turn off focus state
-    focusState = false;
+    closeZoom();
   }
 }
 
-
+svg.selectAll(".thumbs").raise();
+svg.select(".story-image").raise();
 
 // Append the SVG element.
 container.append( svg.node() );
